2a17d6d86c197b7b9459646709aada4866bd96f1
Kstyle0710 <jongbaekim0710@gmail.com>
2020-09-22 07:26:27 +0900

    add lables

--- corona.py
+++ corona.py
@@ -18,11 +18,7 @@ import matplotlib.pyplot as plt
 
 ## 저장한 CSV로 분석 개시
 df = pd.read_csv('./data/seoul_covid19_9_20_.csv', encoding="utf-8")
-df['확진일자'] = pd.to_datetime("2020-"+ df["확진일"].str.replace(".","-"))
-# print(df[["확진일", "확진일자"]].head(10))
-df["월"] = df["확진일자"].dt.month
-df["주"] = df["확진일자"].dt.week
-print(df[["확진일", "확진일자", "월", "주"]].head(10))
+
 
 
 ## 한글 폰트 설정
@@ -30,15 +26,53 @@ import matplotlib.pyplot as plt
 plt.rc("font", family="Malgun Gothic")
 ## 숫자 마이너스 값 깨짐 현장 해결
 plt.rc("axes", unicode_minus=False)
+plt.style.use("fivethirtyeight")
 
 ## 시각화를 더 선명하게 설정
-from IPython.display import set_matplotlib_formats
-set_matplotlib_formats("retina")
+# from IPython.display import set_matplotlib_formats
+# set_matplotlib_formats("retina")
+
+# 날짜 데이터 전처리
+
+df['확진일자'] = pd.to_datetime("2020-"+ df["확진일"].str.replace(".","-"))
+# print(df[["확진일", "확진일자"]].head(10))
+df["월"] = df["확진일자"].dt.month
+df["주"] = df["확진일자"].dt.isocalendar().week
+# print(df[["확진일", "확진일자", "월", "주"]].head(10))
+
+
+## 시각화
+## sort_index의 의미: 시간 순으로 정렬 (데이트타임 타입이면 자동 적용됨)
+# df["확진일자"].value_counts().sort_index().plot(title="Daily Seoul Covid19", figsize=(10, 5))
+# plt.axhline(30, color="red", linestyle=":")
+
+
+## 레이블 표시 (x축에 연도 없이 월-일로 표시하기 - 뒤에서 다섯 글자까지 잘라오기)
+df['월일'] = df["확진일자"].astype(str).map(lambda x : x[-5:])
+# print(df['월일'])
+## 월일은 데이트타임 형식이 아니기 때문에 자동으로 시간 순서로 시각화 안됨
+
+day_count = df['월일'].value_counts().sort_index()
+# print(day_count)
+# day_count.iloc[2]  # x 축의 두번째 값
+
+g= day_count.plot(title="Daily Seoul Covid19", figsize=(20, 5))
+
+# g.text(x=2, y=3, s=3)   # 좌표 2, 3에 레이블 3을 표시 (for문으로 돌리면서 전부 표시 가능)
+for i in range(len(day_count)):
+    case_count = day_count.iloc[i]
+    if case_count > 100:
+        g.text(x=i-0.3, y=case_count, s=case_count)
+
+
+plt.axhline(100, color="red", linestyle=":")
 
 
 
-df["확진일자"].value_counts().sort_index().plot(title="한글 제목", figsize=(10, 5))
-plt.axhline(30, color="red", linestyle=":")
+
+
+
+
 plt.show()
 
 
