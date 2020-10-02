93d76b1a22f595fdf41315eeb0d180f73b3a2345
Kstyle0710 <jongbaekim0710@gmail.com>
2020-10-02 12:47:07 +0900

    dropna, fillna

--- practice1.py
+++ practice1.py
@@ -2,16 +2,26 @@ import pandas as pd
 import numpy as np
 import matplotlib as mtb
 
-df = pd.DataFrame(
-    {"a" : [4 ,5, 6],
-     "b" : [7, 8, 9],
-     "c" : ["aaaaaaaaaaa", "bbbbbbbbbbbbbb", "ccccccccccc"]},
-    index = [1, 2, 3])
-print(df)
-result = df.loc[[1,2], ["a", "c"]]
-print(result)
-df["d"] = df["c"].apply(lambda x : x[:3])
-print(df)
+# df = pd.DataFrame(
+#     {"a" : [4 ,5, 6],
+#      "b" : [7, 8, 9],
+#      "c" : ["aaaaaaaaaaa", "bbbbbbbbbbbbbb", "ccccccccccc"]},
+#     index = [1, 2, 3])
+# print(df)
+# result = df.loc[[1,2], ["a", "c"]]
+# print(result)
+# df["d"] = df["c"].apply(lambda x : x[:3])
+# print(df)
+
+
+
+df2 = pd.DataFrame([[np.nan, 2, np.nan, 0], [3, 4, np.nan, 1], [np.nan, np.nan, np.nan, 5]],
+                   columns=list('ABCD'))
+print(df2)
+
+# df2 = df2.dropna(axis=1, how="any")   ## 열기준으로 하나라도 널값이 있으면 열을 삭제
+df2 = df2.fillna("K")
+print(df2)
 
 
 
